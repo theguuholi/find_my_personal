07b25dd4a99e4c83dbc969f39815ae48b7a914f3
Gustavo Oliveira <g.92oliveira@gmail.com>
2020-08-19 10:39:24 -0300

    resources teacher

---
+++ lib/find_my_personal_web/views/teacher_view.ex
@@ -0,0 +1,21 @@
+defmodule FindMyPersonalWeb.TeacherView do
+  use FindMyPersonalWeb, :view
+  alias FindMyPersonalWeb.TeacherView
+
+  def render("index.json", %{teacher: teacher}) do
+    %{data: render_many(teacher, TeacherView, "teacher.json")}
+  end
+
+  def render("show.json", %{teacher: teacher}) do
+    %{data: render_one(teacher, TeacherView, "teacher.json")}
+  end
+
+  def render("teacher.json", %{teacher: teacher}) do
+    %{id: teacher.id,
+      avatar_url: teacher.avatar_url,
+      name: teacher.name,
+      birth_date: teacher.birth_date,
+      education_level: teacher.education_level,
+      class_type: teacher.class_type}
+  end
+end
---
+++ priv/repo/migrations/20200819133748_create_teacher.exs
@@ -0,0 +1,16 @@
+defmodule FindMyPersonal.Repo.Migrations.CreateTeacher do
+  use Ecto.Migration
+
+  def change do
+    create table(:teacher) do
+      add :avatar_url, :string
+      add :name, :string
+      add :birth_date, :date
+      add :education_level, :string
+      add :class_type, :string
+
+      timestamps()
+    end
+
+  end
+end
