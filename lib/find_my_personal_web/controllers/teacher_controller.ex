359f15089b669972768470c1f301d8d367226314
Gustavo Oliveira <g.92oliveira@gmail.com>
2020-08-26 11:02:22 -0300

    update html and controller

--- lib/find_my_personal_web/controllers/teacher_controller.ex
+++ lib/find_my_personal_web/controllers/teacher_controller.ex
@@ -1,7 +1,7 @@
 defmodule FindMyPersonalWeb.TeacherController do
   use FindMyPersonalWeb, :controller
 
-  alias FindMyPersonal.Teachers
+  alias FindMyPersonal.{Teachers, Teachers.Teacher}
 
   def index(conn, _params) do
     teachers = Teachers.list_teacher()
@@ -13,4 +13,49 @@ defmodule FindMyPersonalWeb.TeacherController do
     render(conn, "show.html", teacher: teacher)
   end
 
+  def new(conn, _params) do
+    changeset = Teachers.change_teacher(%Teacher{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"teacher" => teacher_params}) do
+    case Teachers.create_teacher(teacher_params) do
+      {:ok, teacher} ->
+        conn
+        |> put_flash(:info, "Teacher created successfully.")
+        |> redirect(to: Routes.teacher_path(conn, :show, teacher))
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  def edit(conn, %{"id" => id}) do
+    teacher = Teachers.get_teacher!(id)
+    changeset = Teachers.change_teacher(%Teacher{})
+    render(conn, "edit.html", teacher: teacher, changeset: changeset)
+  end
+
+  def update(conn, %{"id" => id, "teacher" => teacher_params}) do
+    teacher = Teachers.get_teacher!(id)
+
+    case Teachers.update_teacher(teacher, teacher_params) do
+      {:ok, teacher} ->
+        conn
+        |> put_flash(:info, "Teacher updated successfully.")
+        |> redirect(to: Routes.teacher_path(conn, :show, teacher))
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "edit.html", teacher: teacher, changeset: changeset)
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    teacher = Teachers.get_teacher!(id)
+    {:ok, _teacher} = Teachers.delete_teacher(teacher)
+
+    conn
+    |> put_flash(:info, "Teacher deleted successfully.")
+    |> redirect(to: Routes.teacher_path(conn, :index))
+  end
 end
