e51ece1ade9d2d07674cceac9b67cb5f3326da71
Gustavo Oliveira <g.92oliveira@gmail.com>
2020-08-27 10:59:00 -0300

    layout teacher

---
+++ lib/find_my_personal/utils.ex
@@ -0,0 +1,17 @@
+defmodule FindMyPersonal.Utils do
+
+  def age(date) do
+    today = Date.utc_today()
+    age = today.year - date.year
+    month = today.month - date.month
+
+    case month < 0 || (month == 0 && today.day <= date.day) do
+      true -> age - 1
+      false -> age
+    end
+
+  end
+  def convert_br(date) do
+    "#{date.day}/#{date.month}/#{date.year}"
+  end
+end
--- lib/find_my_personal_web/controllers/teacher_controller.ex
+++ lib/find_my_personal_web/controllers/teacher_controller.ex
@@ -19,7 +19,6 @@ defmodule FindMyPersonalWeb.TeacherController do
   end
 
   def create(conn, %{"teacher" => teacher_params}) do
-    IO.inspect teacher_params
     case Teachers.create_teacher(teacher_params) do
       {:ok, teacher} ->
         conn
