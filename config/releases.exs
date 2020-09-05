45960ee08229b4d03b33510075f9000156c0a219
Gustavo Oliveira <g.92oliveira@gmail.com>
2020-09-05 07:26:52 -0300

    update releases.exs

--- config/releases.exs
+++ config/releases.exs
@@ -12,6 +12,20 @@ config :find_my_personal, FindMyPersonal.Repo,
   url: database_url,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "2")
 
+config :find_my_personal, FindMyPersonal.Mail.Mailer,
+  adapter: Bamboo.SMTPAdapter,
+  server: System.get_env("SERVER_HOST_MAIL"),
+  hostname: System.get_env("SERVER_HOST_MAIL"),
+  port: System.get_env("PORT_MAIL"),
+  username: System.get_env("USER_NAME_MAIL"),
+  password: System.get_env("PASSWORD_MAIL"),
+  tls: :if_available,
+  allowed_tls_versions: [:tlsv1, :"tlsv1.1", :"tlsv1.2"],
+  ssl: false,
+  retries: 1,
+  no_mx_lookups: false,
+  auth: :cram_md5
+
 secret_key_base =
   System.get_env("SECRET_KEY_BASE") ||
     raise """
